8be3b99cc26ad74ed2c63cc8c7346ac6cb4f215c
benjamin.hautbois <benjamin.hautbois@lumiscaphe.com>
2016-01-12 09:17:15 +0100

    fixed bootstrap-submodules script

--- bootstrap-submodules.sh
+++ bootstrap-submodules.sh
@@ -5,7 +5,7 @@ git submodule update
 
 for dir in $(find . -type d -maxdepth 1 | grep -e '[0-9]\{3\}'); do
 	echo "Bootstrapping " $dir
-	pushd $dir
+	pushd $dir/love-toys
 	git submodule init
 	git submodule update
 	popd
